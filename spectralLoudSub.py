167f6deabce80b5f868029b6bd0f1ec29b664951
amilo <miloalessia@gmail.com>
2018-12-09 15:29:33 +0000

    added example files

--- spectralLoudSub.py
+++ spectralLoudSub.py
@@ -30,6 +30,7 @@ plt.rcParams.update({'font.size': 10})
 """ short time fourier transform of audio signal """
 def stft(sig, frameSize, overlapFac=0.5, window=np.hanning):
     win = window(frameSize)
+    # commented as python is complaining with floor - changed in int
     # hopSize = int(frameSize - np.floor(overlapFac * frameSize))
     hopSize = int(frameSize - np.int(overlapFac * frameSize))
 
@@ -76,12 +77,14 @@ def logscale_spec(spec, sr=44100, factor=20.):
 """ plot spectrogram"""
 def plotstft(audiopath, binsize=2**10, plotpath=None, colormap="Spectral"):
 
-    samplerate, samples = wav.read(audiopath)
+    #attempt to read 24bit with wavio
     # object = wavio.read(audiopath)
-    #
     # samplerate = object.rate
     # samples = object.data
     # samplerate, width, samples = wavio.read(audiopath)
+
+    samplerate, samples = wav.read(audiopath)
+
     s = stft(samples, binsize)
 
     sshow, freq = logscale_spec(s, factor=1.0, sr=samplerate)
@@ -100,21 +103,19 @@ def plotstft(audiopath, binsize=2**10, plotpath=None, colormap="Spectral"):
     plt.ylim([0, freqbins])
 
 
-
     xlocs = np.float32(np.linspace(0, timebins-1, 32))
     numx = ["%.02f" % l for l in ((xlocs*len(samples)/timebins)+(0.5*binsize))/samplerate]
     xnumbers = [ int(round(float(x))) for x in numx]
     # plt.xticks(xlocs, ["%.02f" % l for l in ((xlocs*len(samples)/timebins)+(0.5*binsize))/samplerate])
     plt.xticks(xlocs, xnumbers)
-    # ylocs = np.int16(np.round(np.linspace(0, freqbins-1, 11)))
-    # ylocs = np.int16(np.round(np.linspace(0, freqbins/2, 11)))
     ylocs = np.int16(np.round(np.linspace(0, freqbins-1, 11)))
     plt.yticks(ylocs, ["%.02f" % freq[i] for i in ylocs])
-    fig.suptitle(audiopath + " - Loudness Subtraction")
-    # axes[1].set_yscale('log')
+    title=audiopath[0:len(audiopath)-4]
+    fig.suptitle( title + " - Loudness Subtraction")
 
     parsedpath = audiopath[0:len(audiopath)-9]
     print parsedpath
+
     # here it needs the suffix of the csv file
     # csvpath = parsedpath + "_vamp_vamp-libxtract_loudness_loudness.csv"
 
@@ -126,14 +127,6 @@ def plotstft(audiopath, binsize=2**10, plotpath=None, colormap="Spectral"):
     y = []
     yl = []
     yr = []
-    # ym = []
-
-    # with open(csvpath,'r') as csvfile:
-    #     plots = csv.reader(csvfile, delimiter=',')
-    #     for row in plots:
-    #         x.append(float(row[0]))
-    #         y.append(float(row[1]))
-
 
     with open(csvpathleft,'r') as csvfileleft:
         plotsleft = csv.reader(csvfileleft, delimiter=',')
@@ -148,40 +141,20 @@ def plotstft(audiopath, binsize=2**10, plotpath=None, colormap="Spectral"):
             y.append(yl[i]-float(row[1]))
             yr.append(float(row[1]))
             i=i+1
-        # with open (csvpathright,'r') as csvfileright:
-        #     plotsright = csv.reader(csvfileright, delimiter=',')
-        #     for row in plotsleft:
-        #         x.append(float(row[0]))
-        #         leftvalue = (float(row[1]))
-        #         rightvalue = (float(plotsright[row][1]))
-        #         y.append(leftvalue-rightvalue)
 
-    # axes[0].plot(x, y, label="subtraction", linestyle='-', linewidth=0.5, markersize=1)
+
     axes[0].plot(x, y, label="subtraction", linestyle='-', linewidth=0.5, markersize=1)
     axes[0].plot(x, yr,label="right", linestyle='--', linewidth=0.2, markersize=1)
     axes[0].plot(x, yl,label="left", linestyle='--', linewidth=0.2, markersize=1)
-    # axes[0].plot(x, yr,label="right", linestyle='--', linewidth=0.2, markersize=1)
-
-
-    # collection = collections.BrokenBarHCollection.span_where(
-    #     x, ymin=0, ymax=2, where=y > 0, facecolor='green', alpha=0.5)
-    # axes[0].add_collection(collection)
-
-    # collection = collections.BrokenBarHCollection.span_where(
-    #     x, ymin=-2, ymax=0, where=y < 0, facecolor='red', alpha=0.5)
-    # axes[0].add_collection(collection)
 
     axes[0].fill_between(x, y, 0, where=y >= 0, alpha = 0.5, facecolor='#d8d8d8')
-    # axes[0].fill_between(x, 0, y, where=y > 0, facecolor='#ff7f0e', interpolate=True)
-    # axes[0].fill_between(x, 0, y, where=y < 0, facecolor='#2ca02c', interpolate=True)
 
     axes[0].set_ylabel('Loudness (sone)')
     axes[0].axis([0, 31, -2, 10])
     axes[0].grid(color='k', linestyle='--', linewidth=0.1)
     axes[0].legend()
 
-    plotpath = audiopath + "Subtraction.png"
-
+    plotpath = audiopath[0:len(audiopath)-4] + "-Subtraction.png"
 
     if plotpath:
 
