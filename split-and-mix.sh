28810bd4ddb307b3424d6992e3def9632a98a66f
Alessia Milo <miloalessia@gmail.com>
2019-03-07 10:31:48 +0000

    Update split-and-mix.sh

--- split-and-mix.sh
+++ split-and-mix.sh
@@ -2,8 +2,8 @@
 
 for f in *.wav;
 do
-  sox "$f" -b 16 "remixed/${f:0:${#f}-4}-left.wav" remix 1
-  sox "$f" -b 16 "remixed/${f:0:${#f}-4}-righ.wav" remix 2
-  sox "$f" -b 16 "remixed/${f:0:${#f}-4}-mono.wav" remix 1,2
+  sox "$f" -b 16 "yourfolder/${f:0:${#f}-4}-left.wav" remix 1
+  sox "$f" -b 16 "yourfolder/${f:0:${#f}-4}-righ.wav" remix 2
+  sox "$f" -b 16 "yourfolder/${f:0:${#f}-4}-mono.wav" remix 1,2
 
 done
